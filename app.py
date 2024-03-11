72cfad9399855036574bcdac1da21bfdb9f1b234
Khushmita Kaur <khushp9894@gmail.com>
2024-03-10 22:56:07 -0700

    add city

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+    city = 'Las Vegas'
+    
+    r = requests.get(url.format(city)).json()
+    print(r)
+    
     return render_template('weather.html')
