c5b60eb92a79804ba1fa52c614875bf06741a165
Khushmita Kaur <khushp9894@gmail.com>
2024-03-12 22:26:16 -0700

    fix errors

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), null=False)
+    name = db.Column(db.String(50), nullable=False)
     
     
     
