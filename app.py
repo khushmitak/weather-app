758531d5d4dfd25efef34eb08ae1e67fad2e48ba
Khushmita Kaur <khushp9894@gmail.com>
2024-03-13 22:55:14 -0700

    add details

--- app.py
+++ app.py
@@ -11,24 +11,27 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     
-    
-    
-    
-    
 @app.route('/')
 def index():
+    cities = City.query.all()
+    
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
     city = 'Las Vegas'
     
-    r = requests.get(url.format(city)).json()
-    print(r)
+    weather_data= []
     
-    weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
-    }
+    for city in cities:
+        
+        r = requests.get(url.format(city)).json()
+    
+        weather = {
+            'city' : city,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+        
+        weather_data.append(weather)
     
     print(weather)
     return render_template('weather.html', weather=weather)
