c9c27b0c73986d3c9afa69c43d0b45bd5df0e617
Khushmita Kaur <khushp9894@gmail.com>
2024-03-14 23:24:38 -0700

    add methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
@@ -11,8 +11,17 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+        
+        if new_city:
+            new_city_obj = City(name=new_city)
+            
+            db.session.add(new_city_obj)
+            db.session.commit()
+            
     cities = City.query.all()
     
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
